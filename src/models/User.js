0dda04c0bea6f3cb49c8882e9d01ad55cfe88604
pravinpandeyit <pravinpandeyit575@gmail.com>
2025-06-08 19:55:46 +0530

    user schema created

---
+++ src/models/User.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+    },
+    is_owner: {
+      type: DataTypes.TINYINT(1),
+      allowNull: false,
+    },
+    is_approved: {
+      type: DataTypes.TINYINT(1),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.TINYINT(1),
+      allowNull: false,
+      defaultValue: 0,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
+);
+
+module.exports = User;
