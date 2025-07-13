1e3f68078731c642f07f0483b0a3cd4dd9543abc
pravinpandeyit <pravinpandeyit575@gmail.com>
2025-07-13 22:34:42 +0530

    changes related to user

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const sequelize = require("./config/database");
 const routes = require("./routes");
 const { generalLimiter } = require("./core/middlewares/rateLimiter");
+const errorHandler = require("./utils/errorHandler");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,8 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(generalLimiter);
 
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api", routes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
